97d4fd446f81fb2ff4a2d6b5f993d231351cbe22
DevNiru2704 <nirmalyabiz2004@gmail.com>
2025-11-15 16:44:22 +0530

    removed unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,6 @@ import ManifestoSection from "./components/home/ManifestoSection";
 import WhatWeMake from "./components/home/WhatWeMake";
 import KeyFigures from "./components/home/KeyFigures";
 import WhoWeAre from "./components/home/WhoWeAre";
-import TestSection from "./components/common/TestSection";
-import ProgressSection from './components/who_we_are/ProcessSection'
 import LetsTalk from "./components/home/LetsTalk";
 import Footer from "./components/common/Footer";
 
